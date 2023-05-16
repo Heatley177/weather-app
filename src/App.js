031a2f34ba46280674e7e0a1b636ed2ae695ef30
Heatley177 <heatleydesign@gmail.com>
2023-05-16 12:39:17 -0400

    Created Weather App utilizing API keys and error animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  
+  const apiKey = 'd366064fd720218ae086c75058de12a4'
+  const [ weatherData, setWeatherData ] = useState([{}])
+  const [ city, setCity ] = useState('')
+  const getWeather = (event) => {
+    if (event.key === 'Enter') {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).then(
+        response => response.json()
+      ).then(
+        data => {
+          setWeatherData(data)
+          setCity('')
+        }
+      )
+    }
+  }
+  function handleClick() {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).then(
+        response => response.json()
+      ).then(
+        data => {
+          setWeatherData(data)
+          setCity('')
+        }
+      )
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Yet Another Weather App</h1>
+      <input 
+      placeholder='Enter City...'
+      className='input'
+      onChange={e => setCity(e.target.value)}
+      value={city}
+      onKeyPress={getWeather}
+      />
+      <button className='button' onClick={handleClick}>Search Weather</button>
+      {typeof weatherData.main === 'undefined' ? (
+        <div>
+          <p>What's the weather like today? Enter in a city to get started.</p>
+        </div>
+      ): (
+        <div className='weather-data'>
+          <p className='city'>{weatherData.name}</p>
+          <p className='temp'>{Math.round(weatherData.main.temp)}°F</p>
+          <p className='weather'>{weatherData.weather[0].main}</p>
+        </div>
+      )}
+
+          {weatherData.cod === '404' ? (
+            <p className='error-message'>City not found. Please check spelling.</p>
+          ): (
+            <>
+            </>
+          )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
